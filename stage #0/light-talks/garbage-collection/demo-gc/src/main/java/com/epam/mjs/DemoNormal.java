f2137c61a4bbb1aaa4a6ece4ad828168a78b0453
Rustam_Lotsmanenko <Rustam_Lotsmanenko@epam.com>
2022-08-21 19:25:24 +0300

    GC light talk + demo

---
+++ stage #0/light-talks/garbage-collection/demo-gc/src/main/java/com/epam/mjs/DemoNormal.java
@@ -0,0 +1,14 @@
+package com.epam.mjs;
+
+public class DemoNormal {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(3000);
+        while (true) {
+            Integer[] integer = new Integer[10000];
+            integer = null;
+            Thread.sleep(10);
+        }
+    }
+
+}
