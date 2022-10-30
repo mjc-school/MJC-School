ece65b35ba2ab8746e3e6000c79712590ae02526
Danila Varatyntsev <danila_varatyntsev@epam.com>
2022-10-30 09:01:54 +0200

    build-tools-LT

---
+++ stage #2/light-talks/build-tools/build-tools-gradle/src/main/java/school/mjc/build/tools/gradle/Main.java
@@ -0,0 +1,11 @@
+package school.mjc.build.tools.gradle;
+
+import com.google.common.base.Strings;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s = Strings.nullToEmpty("Hello World");
+        System.out.println(s);
+    }
+}
