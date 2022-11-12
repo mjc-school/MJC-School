c21000e359c72025597c45605737d0002e42efe0
jndr29384 <108569094+jndr29384@users.noreply.github.com>
2022-11-12 14:44:48 +0200

    Added Code quality tools (#158)

---
+++ stage #2/light-talks/code-quality-tools/src/main/java/com/example/qualitygates/QualityGatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.qualitygates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityGatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QualityGatesApplication.class, args);
+    }
+
+}
